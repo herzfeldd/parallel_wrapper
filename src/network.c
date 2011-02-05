0f58ddb12995b36448f24d06733fd61c52055e59
herzfeldd <herzfeldd@759ea4ef-eb1f-4e7a-9042-8a118faaa79c>
2011-02-05 00:14:38 +0000

    Working on parallel wrapper
    git-svn-id: file:///svn/parallel_wrapper@2 759ea4ef-eb1f-4e7a-9042-8a118faaa79c

--- src/network.c
+++ src/network.c
@@ -8,25 +8,139 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <netdb.h>
+#include "log.h"
+
+#include <pthread.h>
+#include <semaphore.h>
 
 #define PORT 1500
+#define BUFFER_SIZE (1024u)
+
+typedef struct PARALLEL_WRAPPER
+{
+	unsigned int low_port;
+	unsigned int high_port;
+	unsigned int command_port; /**< The assigned command port */
+	int command_socket; /**< The command socket */
+	unsigned int keep_alive_port; /**< The assigned keep alive port */
+	int keep_alive_socket; /**< The keep_alive socket */
+	sem_t sem; /**< Structure semaphore */
+} PARALLEL_WRAPPER;
 
 int get_bound_dgram_socket(int port);
+int port_by_range(struct PARALLEL_WRAPPER *par_wrapper, unsigned int *port, int *socketfd);
+void * process_command(void *);
+
 int main()
 {
-	int i, socketfd;
-	for (i = 1; i < 2048; i++)
+	fd_set readfds;
+	PARALLEL_WRAPPER *par_wrapper = calloc(1, sizeof(struct PARALLEL_WRAPPER));
+	/* Set low range and high range */
+	par_wrapper -> low_port = 1024;
+	par_wrapper -> high_port = 2048;
+	sem_init(&par_wrapper -> sem, 0, 1); /* Semaphore is available */
+
+	/* Open the command port */
+	sem_wait(&par_wrapper -> sem);
+	port_by_range(par_wrapper, &par_wrapper -> command_port, &par_wrapper -> command_socket);
+	port_by_range(par_wrapper, &par_wrapper -> keep_alive_port, &par_wrapper -> keep_alive_socket);
+	sem_post(&par_wrapper -> sem);
+
+
+	FD_ZERO(&readfds);
+	FD_SET(par_wrapper -> command_socket, &readfds);
+	FD_SET(par_wrapper -> keep_alive_socket, &readfds);
+	int n = par_wrapper -> keep_alive_socket + 1;
+	while ( 1 )
 	{
-		socketfd = get_bound_dgram_socket(i);
-		if (socketfd > 0)
+		select(n, &readfds, NULL, NULL, NULL);
+		print(PRNT_INFO, "Returned from select()\n");
+		if (FD_ISSET(par_wrapper -> command_socket, &readfds))
 		{
-			break;
+			pthread_t thread;
+			/* This is the child process */
+			pthread_create(&thread, NULL, &process_command, par_wrapper);
+		}
+		else if (FD_ISSET(par_wrapper -> keep_alive_socket, &readfds))
+		{
+			/* Respond to keep_alive */
 		}
 	}
-	printf("Successfully bound to port %d\n", i);
-	close(socketfd);
+
 	return 0;
 }
+
+void * process_command(void *data)
+{
+	struct sockaddr_storage from;
+	socklen_t from_len = sizeof(struct sockaddr_storage);
+	PARALLEL_WRAPPER *par_wrapper = (PARALLEL_WRAPPER *) data;
+	if (data == (PARALLEL_WRAPPER *) NULL)
+	{
+		return NULL;
+	}
+	print(PRNT_INFO, "Thread started\n");
+	/* Allocate a buffer to hold the information */
+	char *buffer = (char *) malloc(sizeof(char) * BUFFER_SIZE);
+	if (buffer == (char *)NULL)
+	{
+		print(PRNT_ERR, "Unable to allocate buffer for command processing\n");
+		return NULL;
+	}
+	int num_read = recvfrom(par_wrapper -> command_socket, buffer, BUFFER_SIZE, 0,
+		   (struct sockaddr *)&from, &from_len);	
+
+	if (num_read <= 0)
+	{
+		return NULL;
+	}
+
+	printf("Read: %.*s\n", num_read, buffer);
+
+	return NULL;
+}
+
+
+/**
+ * Obtain a port in the range specified by par_wrapper
+ */
+int port_by_range(struct PARALLEL_WRAPPER *par_wrapper, unsigned int *port, int *socketfd)
+{
+	int i;
+	/* Check the validity of par_wrapper */
+	if (par_wrapper == (PARALLEL_WRAPPER *)NULL)
+	{
+		return 1;
+	}
+	if (par_wrapper -> low_port >= par_wrapper -> high_port)
+	{
+		return 2;
+	}
+	/* Check the validity of port and socket */
+	if ((port == (unsigned int *) NULL) || (socketfd == (int *)NULL))
+	{
+		return 3;
+	}
+	for (i = par_wrapper -> low_port; i < par_wrapper -> high_port; i++)
+	{
+		*socketfd = get_bound_dgram_socket(i);
+		if (*socketfd > 0)
+		{
+			*port = i; /* The bound port number */
+			break; /* We bound to a port */
+		}
+	}
+	if (*socketfd <= 0)
+	{
+		print(PRNT_ERR, "Unable to bind to a port in range %d:%d\n", 
+				par_wrapper -> low_port, par_wrapper -> high_port);
+		return 3; /* We failed to bind to a socket */
+	}
+	debug(PRNT_INFO, "Bound port to %d\n", *port);
+
+	return 0;
+}
+
 
 /**
  * Returns the socket descriptor associated with the passed port. 
